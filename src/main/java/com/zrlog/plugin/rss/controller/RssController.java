f0a1d72b05c676d7f8a2a5b3d4af719efbdf1279
xiaochun <90xchun@gmail.com>
2025-08-14 14:12:07 +0800

    Add

--- src/main/java/com/zrlog/plugin/rss/controller/RssController.java
+++ src/main/java/com/zrlog/plugin/rss/controller/RssController.java
@@ -31,12 +31,15 @@ public class RssController {
 
     public void update() {
         session.sendMsg(new MsgPacket(requestInfo.simpleParam(), ContentType.JSON, MsgPacketStatus.SEND_REQUEST, IdUtil.getInt(), ActionType.SET_WEBSITE.name()), msgPacket -> {
+            //更新缓存
+            int msgId = IdUtil.getInt();
+            session.sendJsonMsg(new HashMap<>(), ActionType.REFRESH_CACHE.name(), msgId, MsgPacketStatus.SEND_REQUEST);
+            MsgPacket packetByMsgId = session.getResponseMsgPacketByMsgId(msgId);
+            //response ok
             Map<String, Object> map = new HashMap<>();
-            map.put("success", true);
+            map.put("success", Objects.nonNull(packetByMsgId));
             session.sendMsg(new MsgPacket(map, ContentType.JSON, MsgPacketStatus.RESPONSE_SUCCESS, requestPacket.getMsgId(), requestPacket.getMethodStr()));
             Application.getAutoRefreshFeedFile().doFeed();
-            //更新缓存
-            session.sendJsonMsg(new HashMap<>(), ActionType.REFRESH_CACHE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST);
         });
     }
 
