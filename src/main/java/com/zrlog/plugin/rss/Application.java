c5e05f398f16cd7c17968ff017347f8089ce82b9
xiaochun <90xchun@gmail.com>
2026-05-30 09:07:55 +0800

    Update version

--- src/main/java/com/zrlog/plugin/rss/Application.java
+++ src/main/java/com/zrlog/plugin/rss/Application.java
@@ -6,6 +6,7 @@ import com.zrlog.plugin.render.SimpleTemplateRender;
 import com.zrlog.plugin.rss.controller.RssController;
 import com.zrlog.plugin.rss.handle.AutoRefreshFeedFileRunnable;
 import com.zrlog.plugin.rss.handle.ConnectHandler;
+import com.zrlog.plugin.rss.service.RssRefreshService;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,11 +22,11 @@ public class Application {
     public static void main(String[] args) throws IOException {
         List<Class<?>> classList = new ArrayList<>();
         classList.add(RssController.class);
-        new NioClient(rssConnectHandler, new SimpleTemplateRender(), new RssClientActionHandler()).connectServer(args, classList, RssPluginAction.class);
+        new NioClient(rssConnectHandler, new SimpleTemplateRender(), new RssClientActionHandler())
+                .connectServer(args, classList, RssPluginAction.class, RssRefreshService.class);
     }
 
     public static AutoRefreshFeedFileRunnable getAutoRefreshFeedFile() {
         return rssConnectHandler.getAutoRefreshFeedFile();
     }
 }
-
--- src/main/java/com/zrlog/plugin/rss/handle/ConnectHandler.java
+++ src/main/java/com/zrlog/plugin/rss/handle/ConnectHandler.java
@@ -1,25 +1,16 @@
 package com.zrlog.plugin.rss.handle;
 
 import com.zrlog.plugin.IOSession;
-import com.zrlog.plugin.RunConstants;
 import com.zrlog.plugin.api.IConnectHandler;
 import com.zrlog.plugin.data.codec.MsgPacket;
-import com.zrlog.plugin.type.RunType;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class ConnectHandler implements IConnectHandler {
-
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
     private AutoRefreshFeedFileRunnable autoRefreshFeedFileRunnable;
 
     @Override
     public void handler(IOSession ioSession, MsgPacket msgPacket) {
         this.autoRefreshFeedFileRunnable = new AutoRefreshFeedFileRunnable(ioSession);
-        executorService.scheduleAtFixedRate(autoRefreshFeedFileRunnable, 0, 2, RunConstants.runType == RunType.BLOG ? TimeUnit.MINUTES : TimeUnit.HOURS);
     }
 
     public AutoRefreshFeedFileRunnable getAutoRefreshFeedFile() {
